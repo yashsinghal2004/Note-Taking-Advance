41fd04ecb89275bd02f09a988181871bd65ea2be
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-04-17 00:36:24 +0530

    Heading

--- app/(Landing)/page.tsx
+++ app/(Landing)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex flex-col min-h-full'>
+    <div className='min-h-full flex flex-col'>
       <div className='flex flex-col items-center justify-center md:justify-start
       text-center gap-y-8 flex-1 px-6 pb-10'>
         <Heading/>
--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,8 +1,21 @@
+"use client"
+
+import { ArrowRight } from "lucide-react"
+import { Button } from "./ui/button"
 
 const Heading = () => {
   return (
-    <div>
-      Heading
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents and Plans.Welcome to <span className="underline">NoteBook</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        NoteBook is the connected workspace where <br/> better, faster work happens.
+      </h3>
+      <Button>
+        Enter NoteBook
+        <ArrowRight className="h-4 w-4 ml-2"/>
+      </Button>
     </div>
   )
 }
