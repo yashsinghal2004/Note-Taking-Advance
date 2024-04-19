cf8b2f6998bf7baba53e0fb3dbbb420362af1834
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-04-19 12:06:19 +0530

    Navbar login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,12 @@ import  useScrollTop  from '@/hooks/useScrollTop'
 import { cn } from '@/lib/utils';
 import React from 'react'
 import { ModeToggle } from './mode-toggle';
+import { useConvexAuth } from 'convex/react';
+import { SignInButton } from '@clerk/clerk-react';
+import { Button } from './ui/button';
 
 const Navbar = () => {
+  const {isAuthenticated,isLoading}=useConvexAuth();
     const scroll=useScrollTop();
 
 
@@ -17,7 +21,27 @@ const Navbar = () => {
       <div className='md:ml-auto md:justify-end justify-between w-full items-center flex
       gap-x-4'>
         
-        Login
+        {
+        isLoading &&(
+          <p>Loading...</p>
+        )
+        }
+        {
+          !isAuthenticated && !isLoading && (
+            <>
+            <SignInButton mode='modal'>
+              <Button variant="ghost" size="sm">
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton mode='modal'>
+              <Button size="sm">
+                Get Notebook for free
+              </Button>
+            </SignInButton>
+            </>
+          )
+        }
         <ModeToggle/>
       </div>
       </div>
