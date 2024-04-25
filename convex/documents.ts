e8adfad0ce352dcafa1a147999bfbf2a6497a0db
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-04-26 00:37:06 +0530

    Restore mutation

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,7 +1,8 @@
 
 import { v } from "convex/values"
 import {mutation,query} from "./_generated/server"
-import { Id } from "./_generated/dataModel";
+import { Doc, Id } from "./_generated/dataModel";
+import { urlToHttpOptions } from "url";
 
 
 export const archive=mutation({
@@ -106,3 +107,81 @@ export const create=mutation({
         return document;
     }
 })
+
+export const getTrash=query({
+    handler:async(ctx)=>{
+        const identity=await ctx.auth.getUserIdentity();
+        if(!identity){
+            throw new Error("Not Authenticated")
+        }
+
+        const userId=identity.subject;
+
+        const documents=await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent",(q)=>(
+            q.eq("userId",userId)
+        ))
+        .filter((q)=>
+        q.eq(q.field("isArchived"),true))
+        .order("desc").collect();
+
+        return documents;
+    }
+})
+
+export const restore=mutation({
+    args:{id:v.id("documents")},
+    handler:async(ctx,args)=>{
+        const identity=await ctx.auth.getUserIdentity();
+        if(!identity){
+            throw new Error("Not Authenticated")
+        }
+
+        const userId=identity.subject;
+
+        const existingDocument=await ctx.db.get(args.id);
+        if(!existingDocument){
+            throw new Error("Not Found");
+        }
+
+        if(existingDocument.userId!==userId){
+            throw new Error("Unauthorized");
+        }
+
+        const recursiveRestore=async(documentId:Id<"documents">)=>{
+            const children=await ctx.db
+            .query("documents")
+            .withIndex("by_user_parent",(q)=>(
+                q
+               .eq("userId",userId)
+               .eq("parentDocument",documentId)
+            ))
+            .collect();
+
+            for(const child of children) {
+                await ctx.db.patch(child._id,{
+                    isArchived:false,
+                });
+                await recursiveRestore(child._id);
+            }
+        }
+
+        const options:Partial<Doc<"documents">> = {
+            isArchived:false,
+        }
+
+        if(existingDocument.parentDocument){
+            const parent=await ctx.db.get(existingDocument.parentDocument);
+            if(parent?.isArchived){
+                options.parentDocument = undefined;
+            }
+        }
+
+        await ctx.db.patch(args.id,options);
+
+        recursiveRestore(args.id);
+        return existingDocument;
+    }
+})
+
