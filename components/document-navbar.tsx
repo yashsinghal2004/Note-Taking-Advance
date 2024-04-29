960f34ca572e85fee4968c237a1441bca6509a7f
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-04-30 00:27:23 +0530

    navbar and rename document feature

--- components/document-navbar.tsx
+++ components/document-navbar.tsx
@@ -5,6 +5,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
+import { Title } from "./title";
 
 interface NavbarProps{
     isCollapsed:boolean;
@@ -17,15 +18,16 @@ export const Navbar = ({
 }:NavbarProps) => {
     const params=useParams();
 
-    const documents=useQuery(api.documents.getById,{
+    const document=useQuery(api.documents.getById,{
         documentId:params.documentId as Id<"documents">,
     });
 
-    if(documents===null){
+    if(document===null){
         return null;
     }
 
     return ( 
+        <>
         <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 
         w-full flex items-center gap-x-4">
             {
@@ -38,9 +40,12 @@ export const Navbar = ({
                     />
                 )
             }
-            <div className="flex items-center justify-between w-full">Navbar</div>
+            <div className="flex items-center justify-between w-full">
+                <Title initialData={document}/>
+            </div>
 
         </nav>
+        </>
      ); 
 }
  
