0f5952d293eb591116e792c7b05c1e5bec1214fb
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-05-02 00:35:33 +0530

    Emoji feature

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
+import { Button } from "./ui/button";
+import { Smile, X } from "lucide-react";
 
 interface ToolbarProps{
     initialData:Doc<"documents">;
@@ -16,9 +19,39 @@ export const Toolbar = ({
             {!!initialData.icon && !preview &&(
                 <div className="flex items-center gap-x-2 
                 group/icon pt-6">
-
+                    <IconPicker onChange={()=>{}}>
+                        <p className="text-6xl hover:opacity-75 transition">
+                            {initialData.icon}
+                            </p>
+                    </IconPicker>
+                    <Button
+                    onClick={()=>{}}
+                    className="rounded-full opacity-0 group-hover/icon:opacity-100 
+                    transition text-muted-foreground text-xs"
+                    variant="outline"
+                    size="icon"
+                    >
+                        <X className="w-4 h-4" />
+                    </Button>
                 </div>
             )}
+            {!!initialData.icon && preview &&(
+                <p className="text-6xl pt-6">
+                    {initialData.icon}
+                </p>
+            )}
+            <div className="opacity-100 group-hover:opacity-100
+            flex items-center gap-x-1 py-4">
+                <IconPicker asChild onChange={()=>{}}>
+                    <Button
+                    className="text-muted-foreground text-xs"
+                    variant="outline"
+                    size="sm"
+                    />
+                    <Smile className="h-4 w-4 mr-2"/>
+                    Add icon 
+                </IconPicker>
+            </div>
         </div>
 
      );
