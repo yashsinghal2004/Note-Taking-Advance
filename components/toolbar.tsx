204309fb48a03e9c30801e511195e343a5b10c7c
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-05-02 19:04:18 +0530

    Toolbar updated

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -3,7 +3,10 @@
 import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./icon-picker";
 import { Button } from "./ui/button";
-import { Smile, X } from "lucide-react";
+import { ImageIcon, Smile, X } from "lucide-react";
+import { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 interface ToolbarProps{
     initialData:Doc<"documents">;
@@ -14,6 +17,33 @@ export const Toolbar = ({
     initialData,
     preview,
 }:ToolbarProps) => {
+    const inputRef=useRef<ElementRef<"textarea">>(null);
+    const [isEditing,setIsEditing]=useState(false);
+    const [value,setValue]=useState(initialData.title);
+
+    const update=useMutation(api.documents.update);
+
+    const enableInput=()=>{
+        if(preview)return;
+
+        setIsEditing(true);
+        setTimeout(() => {
+            setValue(initialData.title);
+            inputRef.current?.focus();
+        }, 0);
+    }
+
+    const disableInput=()=>setIsEditing(false);
+
+    const onInput=(value:string)=>{
+        setValue(value);
+        update({
+            id:initialData._id,
+            title:value || "Untitled"
+        });
+    };
+
+    
     return ( 
         
         <div className="pl-[54px] group relative">
@@ -54,6 +84,17 @@ export const Toolbar = ({
                     Add icon 
                     </Button>
                 </IconPicker>
+                )}
+                {!initialData.coverImage && !preview &&(
+                    <Button
+                    onClick={()=>{}}
+                    className="text-muted-foreground text-xs"
+                    variant="outline"
+                    size="sm"
+                    >
+                        <ImageIcon className="h-4 w-4 mr-2"/>
+                        Add cover
+                    </Button>
                 )}
                 
             
