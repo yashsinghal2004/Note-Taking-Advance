b3fdcab7388cb789eda6dc5f75d5bc907d44bde6
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-05-26 14:20:37 +0530

    Toolbar, add icon feature, cover image hook

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -7,6 +7,7 @@ import { ImageIcon, Smile, X } from "lucide-react";
 import { ElementRef, useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import TextareaAutosize from "react-textarea-autosize"
 
 interface ToolbarProps{
     initialData:Doc<"documents">;
@@ -22,6 +23,7 @@ export const Toolbar = ({
     const [value,setValue]=useState(initialData.title);
 
     const update=useMutation(api.documents.update);
+    const removeIcon=useMutation(api.documents.removeIcon);
 
     const enableInput=()=>{
         if(preview)return;
@@ -43,20 +45,41 @@ export const Toolbar = ({
         });
     };
 
+    const onKeyDown=(
+        event:React.KeyboardEvent<HTMLTextAreaElement>
+    )=>{
+        if(event.key==="Enter"){
+            event.preventDefault();
+            disableInput();
+        }
+    }
     
+    const onIconSelect=(icon:string)=>{
+        update({
+            id: initialData._id,
+            icon,
+        });
+    };
+
+    const onRemoveIcon=()=>{
+        removeIcon({
+            id:initialData._id,
+        });
+    };
+
     return ( 
         
         <div className="pl-[54px] group relative">
             {!!initialData.icon && !preview &&(
                 <div className="flex items-center gap-x-2 
                 group/icon pt-6">
-                    <IconPicker onChange={()=>{}}>
+                    <IconPicker onChange={onIconSelect}>
                         <p className="text-6xl hover:opacity-75 transition">
                             {initialData.icon}
                             </p>
                     </IconPicker>
                     <Button
-                    onClick={()=>{}}
+                    onClick={onRemoveIcon}
                     className="rounded-full opacity-0 group-hover/icon:opacity-100 
                     transition text-muted-foreground text-xs"
                     variant="outline"
@@ -74,7 +97,7 @@ export const Toolbar = ({
             <div className="opacity-100 group-hover:opacity-100
             flex items-center gap-x-1 py-4">
                 {!initialData.icon && !preview && (
-                    <IconPicker asChild onChange={()=>{}}>
+                    <IconPicker asChild onChange={onIconSelect}>
                     <Button
                     className="text-muted-foreground text-xs"
                     variant="outline"
@@ -99,6 +122,24 @@ export const Toolbar = ({
                 
             
             </div>
+            {isEditing && !preview ?(
+                <TextareaAutosize
+                ref={inputRef}
+                onBlur={disableInput}
+                onKeyDown={onKeyDown}
+                value={value}
+                onChange={(e)=>onInput(e.target.value)}
+                className="text-5xl bg-transparent font-bold break-words
+                outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"/>
+            ):(
+                <div
+                onClick={enableInput}
+                className="pb-[11.5px] text-5xl font-bold break-words
+                outline-none text-[#3F3F3F] dark:text-[#CFCFCF]"
+                >
+                    {initialData.title}
+                </div>
+            )}
         </div>
 
      );
