2b3e77b771d96d783ade3bb5bc290ff3f4624b4d
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-06-05 08:14:48 +0530

    updated

--- components/models/cover-image-modal.tsx
+++ components/models/cover-image-modal.tsx
@@ -2,9 +2,47 @@
 
 import { useCoverImage } from "@/hooks/use-cover-image"
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+import { SingleImageDropzone } from "../single-image-dropzone";
 
  export const CoverImageModal=()=>{
+    const params=useParams();
+    const update=useMutation(api.documents.update);
+
+    const [file,setFile]=useState<File>();
+    const [isSubmitting,setIsSubmitting]=useState(false);
     const coverImage =useCoverImage();
+    const {edgestore}=useEdgeStore();
+
+    const onClose=()=>{
+        setFile(undefined);
+        setIsSubmitting(false);
+        coverImage.onClose();
+    }
+
+    const onChange=async(file?:File)=>{
+        if(file){
+            setIsSubmitting(true);
+            setFile(file);
+        
+        
+        const res= await edgestore.publicFiles.upload({
+            file
+        });
+
+        await update({
+            id:params.documentId as Id<"documents">,
+            coverImage:res.url
+        });
+
+        onClose();
+    }}
+
 
     return(
         <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
@@ -14,9 +52,12 @@ import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
                         Cover Image
                     </h2>
                 </DialogHeader>
-                <div>
-                    TODO: Upload image
-                </div>
+                <SingleImageDropzone
+                className="w-full outline-none"
+                disabled={isSubmitting}
+                value={file}
+                onChange={onChange}
+                />
             </DialogContent>
         </Dialog>
     )
